096ec735815ea81f01cbae77949ccfa3e37b2655
prashanth avvari <apprashanth55@gmai.com>
2021-06-03 22:03:03 +0530

    set visited key

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var isAlertClosed = false; //var such that all files can access this variable
-let game = new Canvas();
-game.run();
+
+window.onload = () => {
+  var isAlertClosed = window.localStorage.getItem('isVisited') || false; //var such that all files can access this variable
+  isAlertClosed && closeAlert();
+  let game = new Canvas();
+  game.run();
+}
 
 function closeAlert() {
   document.querySelector('#alert').remove();
+  window.localStorage.setItem('isVisited', true);
   isAlertClosed = true;
 }
