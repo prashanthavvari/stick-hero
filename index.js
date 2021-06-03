19ce35fe91e18af6e2a89214b456d9d8dd122ad2
prashanth avvari <apprashanth55@gmai.com>
2021-06-03 21:47:58 +0530

    add alert box

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+var isAlertClosed = false; //var such that all files can access this variable
 let game = new Canvas();
 game.run();
+
+function closeAlert() {
+  document.querySelector('#alert').remove();
+  isAlertClosed = true;
+}
